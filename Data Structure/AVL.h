2e41a32217112b60f46a7ba6ff220afe03f80b69
IftikharIfti <98595020+IftikharIfti@users.noreply.github.com>
2022-11-01 18:24:14 +0600

    Add files via upload

---
+++ Data Structure/AVL.h
@@ -0,0 +1,132 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+// An AVL tree node
+class Node
+{
+    public:
+    int key;
+    Node *left;
+    Node *right;
+    int height;
+};
+int max(int a, int b);
+ 
+int height(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+Node* newNode(int key)
+{
+    Node* node = new Node();
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+    return(node);
+}
+Node *rightRotate(Node *y)
+{
+    Node *x = y->left;
+    Node *T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    x->height = max(height(x->left),
+                    height(x->right)) + 1;
+ 
+    // Return new root
+    return x;
+}
+Node *leftRotate(Node *x)
+{
+    Node *y = x->right;
+    Node *T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = max(height(x->left),   
+                    height(x->right)) + 1;
+    y->height = max(height(y->left),
+                    height(y->right)) + 1;
+    return y;
+}
+int Balance_Factor(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+Node* insert(Node* node, int key)
+{
+    if (node == NULL)
+        return(newNode(key));
+ 
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else 
+        return node;
+    node->height = 1 + max(height(node->left),
+                        height(node->right));
+ 
+  
+    int balance = Balance_Factor(node);
+ 
+    // If this node becomes unbalanced, then
+    // there are 4 cases
+ 
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+ 
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+ 
+    // Left Right Case
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+ 
+    // Right Left Case
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+    return node;
+}
+Node* bst_insert(Node* node,int key)
+{ 
+    if (node == NULL)
+        return(newNode(key));
+ 
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+     
+        return node;
+}
+
+void InOrder(Node *root)
+{
+    if(root != NULL)
+    {
+        InOrder(root->left);
+        cout << root->key <<"("<<Balance_Factor(root)<<") ";
+        InOrder(root->right);
+    }
+}
